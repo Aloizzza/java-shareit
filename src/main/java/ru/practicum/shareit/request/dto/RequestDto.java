592669a2a05a2149bf791257ff0bad56ddddf936
Aloizzza <aloizzza@yandex.ru>
2022-12-05 02:01:20 +0300

    refactor: финальный проект 15 спринта (исправления 1)+удаление опечатки

--- src/main/java/ru/practicum/shareit/request/dto/RequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/RequestDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.request.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.practicum.shareit.item.dto.ItemDto;
 
